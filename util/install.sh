6c8fe1ac018a2237225947715867014a30308e7c
Like-all <lik3a11@gmail.com>
2014-04-11 18:51:01 +0400

    Changes in sed expressions

--- util/install.sh
+++ util/install.sh
@@ -36,24 +36,24 @@ cd /opt/btce-mobile/ && mv settings.js.example settings.js
 
 echo "Type your username:"
 read site_username
-sed -i 's/site_username/'$site_username'/' /opt/btce-mobile/settings.js
+sed -i 's|site_username|'$site_username'|' /opt/btce-mobile/settings.js
 
 echo "Type your password:"
 read -s site_password
-sed -i 's/site_password/'$site_password'/' /opt/btce-mobile/settings.js
+sed -i 's|site_password|'$site_password'|' /opt/btce-mobile/settings.js
 
 echo "Type your BTCe api key:"
 read btc_e_api_key
-sed -i 's/btc-e_api_key/'$btc_e_api_key'/' /opt/btce-mobile/settings.js
+sed -i 's|btc-e_api_key|'$btc_e_api_key'|' /opt/btce-mobile/settings.js
 
 echo "Type your BTCe api secret:"
 read btc_e_api_secret
-sed -i 's/btc-e_api_secret/'$btc_e_api_secret'/' /opt/btce-mobile/settings.js
+sed -i 's|btc-e_api_secret|'$btc_e_api_secret'|' /opt/btce-mobile/settings.js
 
 echo "Type path to certificate:"
 read path_to_cert
-sed -i 's/path_to_cert.pem/'$path_to_cert'/' /opt/btce-mobile/settings.js
+sed -i 's|path_to_cert.pem|'$path_to_cert'|' /opt/btce-mobile/settings.js
 
 echo "Type path to private key:"
 read path_to_privkey
-sed -i 's/path_to_cert.key/'$path_to_privkey'/' /opt/btce-mobile/settings.js
+sed -i 's|path_to_cert.key|'$path_to_privkey'|' /opt/btce-mobile/settings.js
